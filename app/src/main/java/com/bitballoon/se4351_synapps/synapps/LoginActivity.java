741ea0dd510efd56506b7d6ab5cb4b55fe3ddddb
Brandon Quiocho <brandonquiocho@Brandons-MBP-3.home>
2015-09-18 21:39:05 -0500

    Added new_user_icons page with associated client and loved one icons. Changed the color value of the Synapse bar text color.

--- app/src/main/java/com/bitballoon/se4351_synapps/synapps/LoginActivity.java
+++ app/src/main/java/com/bitballoon/se4351_synapps/synapps/LoginActivity.java
@@ -11,6 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //setContentView(R.layout.new_user_icons);
     }
 
     @Override
