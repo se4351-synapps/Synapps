ba4088e22e708fe9127fcadd5a535f97cf8f2e37
Brandon Quiocho <brandonquiocho@Brandons-MBP-3.home>
2015-09-18 23:11:15 -0500

    Updated the NewUserSelectionActivity. Added functionality to each icon.

--- app/src/main/java/com/bitballoon/se4351_synapps/synapps/NewUserSelectionActivity.java
+++ app/src/main/java/com/bitballoon/se4351_synapps/synapps/NewUserSelectionActivity.java
@@ -1,9 +1,11 @@
 package com.bitballoon.se4351_synapps.synapps;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -18,6 +20,8 @@ public class NewUserSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_user_icons);
+        new_patient_icon_click();
+        new_loved_one_icon_click();
     }
 
     public void new_patient_icon_click() {
@@ -25,8 +29,20 @@ public class NewUserSelectionActivity extends AppCompatActivity {
         patient_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(NewUserSelectionActivity.this, "Hello", Toast.LENGTH_LONG).show();
+                Toast.makeText(NewUserSelectionActivity.this, "Ask user to get their loved one", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+    public void new_loved_one_icon_click() {
+        loved_one_image = (ImageView)findViewById(R.id.loved_one_icon);
+        loved_one_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(NewUserSelectionActivity.this, "Go to New User Account page", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
