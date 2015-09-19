9789a0f3c81a51544045a7f1e3e66ecd06cf7c83
Bontavy Vorng <bontavy14@hotmail.com>
2015-09-18 22:17:04 -0500

    Go from login screen to new user selection screen when clicking on New User

---
+++ app/src/main/java/com/bitballoon/se4351_synapps/synapps/CreateNewUserActivity.java
@@ -0,0 +1,7 @@
+package com.bitballoon.se4351_synapps.synapps;
+
+/**
+ * Created by Bontavy on 9/18/2015.
+ */
+public class CreateNewUserActivity {
+}
--- app/src/main/java/com/bitballoon/se4351_synapps/synapps/LoginActivity.java
+++ app/src/main/java/com/bitballoon/se4351_synapps/synapps/LoginActivity.java
@@ -1,16 +1,34 @@
 package com.bitballoon.se4351_synapps.synapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button newUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //setContentView(R.layout.new_user_icons);
+        selectNewUser();
     }
+
+    public void selectNewUser() {
+        newUserButton = (Button)findViewById(R.id.new_user_button);
+        newUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, NewUserSelectionActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
 }
