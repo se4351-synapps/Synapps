b24a957d954830c44695f84d30384b9e61816462
Quang Anh Phan <pquanganh@gmail.com>
2015-11-08 16:40:39 -0600

    Emergency Call

--- app/src/main/java/com/bitballoon/se4351_synapps/synapps/CreateNewUserActivity.java
+++ app/src/main/java/com/bitballoon/se4351_synapps/synapps/CreateNewUserActivity.java
@@ -3,6 +3,9 @@ package com.bitballoon.se4351_synapps.synapps;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -55,5 +58,25 @@ public class CreateNewUserActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.emergency_call, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_call:
+                // User chose the "Call" item, show the app settings UI...
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
--- app/src/main/java/com/bitballoon/se4351_synapps/synapps/MainMenuActivity.java
+++ app/src/main/java/com/bitballoon/se4351_synapps/synapps/MainMenuActivity.java
@@ -20,15 +20,15 @@ public class MainMenuActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_login, menu);
+        inflater.inflate(R.menu.emergency_call, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_settings:
-                // User chose the "Settings" item, show the app settings UI...
+            case R.id.action_call:
+                // User chose the "Call" item, show the app settings UI...
                 return true;
             default:
                 // If we got here, the user's action was not recognized.
