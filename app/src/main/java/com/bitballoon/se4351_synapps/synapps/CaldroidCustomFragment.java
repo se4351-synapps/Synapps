7a997a197a1303d79984f3d1ab53c692cbd86545
Andrew Pham <phoshizzle35@gmail.com>
2015-11-17 23:02:03 -0600

    Imported module Caldroid for calendar implementation since default CalendarView is garbage. Need to store all notifications in an accessible data structure to generate the notification count in the calendar.

---
+++ app/src/main/java/com/bitballoon/se4351_synapps/synapps/CaldroidCustomFragment.java
@@ -0,0 +1,7 @@
+package com.bitballoon.se4351_synapps.synapps;
+
+/**
+ * Created by virtualbloks on 11/14/15.
+ */
+public class CaldroidCustomFragment {
+}
---
+++ app/src/main/java/com/bitballoon/se4351_synapps/synapps/CalendarActivity.java
@@ -0,0 +1,47 @@
+package com.bitballoon.se4351_synapps.synapps;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calender);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.emergency_call, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_call:
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:2816850685"));
+                startActivity(callIntent);
+                // User chose the "Call" item, show the app settings UI...
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
